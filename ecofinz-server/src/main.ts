b2e518ebe01e9fc068aaebc0e9428e4ec4fc662f
Rest <code.rest.dev@gmail.com>
2025-12-07 00:10:46 -0300

    login(backend): Modificación de servidor cors para recibir peticiónes de front

--- ecofinz-server/src/main.ts
+++ ecofinz-server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // La URL de tu frontend
+    origin: 'http://localhost:3000', // La URL de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
